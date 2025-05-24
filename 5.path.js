460a336595f76ab9d5ce861c8b2545dd5d5b70a7
Eduardo <cutisunr@gmail.com>
2025-05-24 17:25:49 -0300

    Eliminando archivos viejos

--- 5.path.js
+++
@@ -1,9 +0,0 @@
-const path = require('node:path')
-
-// preguntar barra separadora segun SO
-console.log(path.sep)
-
-// unir rutas con path.join
-const filePath = path.join('content','subfolder','test.txt')
-
-console.log(filePath)
